a01acd48aa7a0f2222550a7caafb6819f2fc3e48
Martin STEFFEN <msteffen.externe@bedrockstreaming.com>
2021-12-02 16:49:15 +0100

    init

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZrbbvL4SEFx6kYkv5fHe8Y8PXdp-g1lw",
+    authDomain: "shopify-b5394.firebaseapp.com",
+    projectId: "shopify-b5394",
+    storageBucket: "shopify-b5394.appspot.com",
+    messagingSenderId: "1070954135438",
+    appId: "1:1070954135438:web:4399632dac63cce554aedf",
+    measurementId: "G-ZL7C6GKFLN"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
