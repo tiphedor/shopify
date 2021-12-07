e8ccac1c5981ff458305b416656907eb011950c9
Martin STEFFEN <mail@tiph.io>
2021-12-07 23:38:05 +0000

    Eslint! (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,97 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import {
+	selectIsUserLoading,
+	selectIsUserSignedIn,
+	selectUserError,
+	signInUserWithPassword,
+	signInWithGoogle,
+	signOutUser,
+	signUpUserWithPassword,
+} from './features/user/userSlice';
 
-function App() {
-  return (
-    <div className="App">
-      Test :)
-    </div>
-  );
-}
+const App: React.FC = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const isSignedIn = useAppSelector(selectIsUserSignedIn);
+	const isLoading = useAppSelector(selectIsUserLoading);
+	const userError = useAppSelector(selectUserError);
+	const dispatch = useAppDispatch();
+
+	const handleFieldChange = useCallback(
+		(updateFn) => (e: ChangeEvent<HTMLInputElement>) => {
+			updateFn(e.currentTarget.value);
+		},
+		[]
+	);
+
+	const handleGoogleSignIn = () => dispatch(signInWithGoogle());
+	const handleEmailPasswordSignIn = () =>
+		dispatch(signInUserWithPassword({ email, password }));
+	const handleEmailPasswordSignUp = () =>
+		dispatch(signUpUserWithPassword({ email, password }));
+	const handleSignOut = () => dispatch(signOutUser());
+
+	return (
+		<div className="App">
+			<code>
+				isSignedIn: {JSON.stringify(isSignedIn)} <br />
+				loading: {JSON.stringify(isLoading)} <br />
+				error: {JSON.stringify(userError)} <br />
+				Form data: <br />
+				username: {email} <br />
+				password: {password} <br />
+			</code>
+
+			<br />
+			<br />
+			<br />
+			<br />
+			<button onClick={handleSignOut}>signout</button>
+
+			<h1>Sign In</h1>
+
+			<br />
+			<br />
+
+			<label htmlFor="email">Email Address</label>
+			<input
+				id="email"
+				name="email"
+				type="text"
+				disabled={isLoading}
+				onChange={handleFieldChange(setEmail)}
+			/>
+
+			<br />
+
+			<label htmlFor="password">Password</label>
+			<input
+				id="password"
+				name="password"
+				type="password"
+				disabled={isLoading}
+				onChange={handleFieldChange(setPassword)}
+			/>
+
+			<br />
+
+			<button disabled={isLoading} onClick={handleEmailPasswordSignIn}>
+				Sign in
+			</button>
+			<button disabled={isLoading} onClick={handleEmailPasswordSignUp}>
+				Sign Up
+			</button>
+
+			<h1>Sign in with a social provider</h1>
+
+			<button onClick={handleGoogleSignIn} disabled={isLoading}>
+				Google
+			</button>
+		</div>
+	);
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import App from './App';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDZrbbvL4SEFx6kYkv5fHe8Y8PXdp-g1lw",
-    authDomain: "shopify-b5394.firebaseapp.com",
-    projectId: "shopify-b5394",
-    storageBucket: "shopify-b5394.appspot.com",
-    messagingSenderId: "1070954135438",
-    appId: "1:1070954135438:web:4399632dac63cce554aedf",
-    measurementId: "G-ZL7C6GKFLN"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-/* eslint-disable-next-line */
-const analytics = getAnalytics(firebaseApp);
+import { store } from './app/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
